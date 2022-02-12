30b9e76adb69f892bd3f9837062bfba3dbc247a3
MajorChe <charithnanduri03@gmail.com>
2022-02-12 10:34:52 -0500

    add firebase config

---
+++ src/firebase/Config.js
@@ -0,0 +1,23 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_KEY,
+  authDomain: process.env.REACT_APP_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE,
+  messagingSenderId: process.env.REACT_APP_MESSAGEID,
+  appId: process.env.REACT_APP_ID,
+};
+
+//initialize firebase
+firebase.initializeApp(firebaseConfig);
+
+//initialize service
+const projectTicketTracker = firebase.firestore();
+const projectTicketTrackerAuth = firebase.auth();
+
+const timestamp = firebase.firestore.Timestamp
+
+export { projectTicketTracker, projectTicketTrackerAuth,timestamp };
