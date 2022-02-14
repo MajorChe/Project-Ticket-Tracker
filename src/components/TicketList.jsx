d1cb75d4dfba8d5b21f62cab34be8588a8c51237
MajorChe <charithnanduri03@gmail.com>
2022-02-14 18:01:56 -0500

    refactor ticket list component

--- src/components/TicketList.jsx
+++ src/components/TicketList.jsx
@@ -1,36 +1,19 @@
-import { Avatar, Flex, Stack, Text } from "@chakra-ui/react";
+import { Flex, Text } from "@chakra-ui/react";
 import React from "react";
+import TicketListItem from "./TicketListItem";
 
 const TicketList = () => {
   return (
-    <Stack direction={"row"} wrap="wrap" spacing={"10"}>
-      <Flex direction={"column"} gap="3" align={"center"} wrap="wrap">
-        <Text fontWeight={600} fontSize="lg">
-          Name
-        </Text>
-        <Text fontSize={"md"}>1. Project 1</Text>
+    <Flex direction={"column"}>
+      <Flex direction={"row"} gap={3} textAlign={"center"} wrap="wrap" px="5">
+        <Text fontWeight={500} fontSize="lg">Name</Text>
+        <Text fontWeight={500} fontSize="lg" flex="1">Description</Text>
+        <Text fontWeight={500} fontSize="lg">Author</Text>
       </Flex>
-      <Flex
-        direction={"column"}
-        gap={3}
-        align={"center"}
-        flex="1"
-        display={{ base: "flex", md: "flex" }}
-      >
-        <Text fontWeight={600} fontSize="lg">
-          Description
-        </Text>
-        <Text fontSize={"md"}>
-          Lorem ipsum dolor sit amet, consectetur adipisicing eyshairty
-        </Text>
-      </Flex>
-      <Flex direction={"column"} gap={3} align={"center"}>
-        <Text fontWeight={600} fontSize="lg">
-          Author
-        </Text>
-        <Avatar size={"sm"} src="https://bit.ly/ryan-florence" />
-      </Flex>
-    </Stack>
+      <TicketListItem/>
+      <TicketListItem/>
+      <TicketListItem/>
+    </Flex>
   );
 };
 
